8d80e414b98906091e4bda00458306c225ed369b
mv23c <100789090+mv23c@users.noreply.github.com>
2023-01-13 15:36:28 -0300

    Subindo o projeto

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='http://github.com/mv23c.png' alt='Mário Vinícius' />
+            </div>
+            <div className='rodape'>
+                <h4>Mário Vinícius</h4>
+                <h5>Programador</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador;
---
+++ src/componentes/Time/index.js
@@ -0,0 +1,16 @@
+import Colaborador from '../Colaborador';
+import './Time.css'
+
+const Time = (props) => {
+const css = { backgroundColor: props.corSecundaria }
+
+    return (
+        <section className='time' style={css}>
+            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+            <Colaborador />
+            <Colaborador />
+        </section>
+    );
+}
+
+export default Time;
